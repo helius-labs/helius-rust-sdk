dbfcf009cd43a1acba3cc845be88b40fc51af292
Evan <0xIchigo@protonmail.com>
2024-04-17 16:11:51 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod client;
+mod config;
+mod das_api;
+mod mint_api;
+mod webhook;
+mod rpc;
+mod utils;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use client::Helius;
