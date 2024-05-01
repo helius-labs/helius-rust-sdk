90cc324c1f0da355c8c98495c275b1d0d0c5c5a9
Evan <0xIchigo@protonmail.com>
2024-05-01 13:13:39 -0400

    Better Documentation For request_handler.rs

--- src/request_handler.rs
+++ src/request_handler.rs
@@ -4,23 +4,55 @@ use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 use std::sync::Arc;
 
+/// Manages HTTP requests for the `Helius` client
+///
+/// This struct is responsible for sending HTTP requests and handling responses. It encapsulates details
+/// of the `reqwest::Client` to provide a simplified interface for making requests and processing responses
 #[derive(Clone)]
 pub struct RequestHandler {
     pub http_client: Arc<Client>,
 }
 
 impl RequestHandler {
-    /// Initializes a new RequestHandler instance
+    /// Creates a new instance of `RequestHandler`
+    ///
+    /// # Arguments
+    /// * `client` - A shared instance of a `reqwest::Client`
     pub fn new(client: Arc<Client>) -> Result<Self> {
         Ok(Self { http_client: client })
     }
 
-    /// Sends a given request via the RequestBuilder and returns a Response
+    /// Asynchronously sends a specified HTTP request using the `RequestBuilder`
+    ///
+    /// # Arguments
+    /// * `request_builder` - Configured `RequestBuilder` for a specific HTTP request
+    ///
+    /// # Returns
+    /// A `Result` wrapping a `reqwest::Response` if the request is send and received successfully
+    ///
+    /// # Errors
+    /// Returns `HeliusError::Network` if there is an issue sending the request
     async fn send_request(&self, request_builder: RequestBuilder) -> Result<Response> {
         request_builder.send().await.map_err(HeliusError::Network)
     }
 
-    /// Handles the send functionality for a given HTTP request
+    /// Sends an HTTP request and processes the response to deserialize into a specified generic type
+    ///
+    /// # Type Parameters
+    /// * `R` - The type of the request body, which must implement `Serialize`
+    /// * `T` - The expected type of the response, which must implement `Deserialize`
+    ///
+    /// # Arguments
+    /// * `method` - The HTTP method to be used for the request
+    /// * `url` - The URL to which the request is sent
+    /// * `body` - An optional request body, serialized as JSON if provided
+    ///
+    /// # Returns
+    /// A `Result` wrapping the deserialized response data if the response is successful
+    ///
+    /// # Errors
+    /// Returns an error if the request fails at any stage, including network errors, serialization errors
+    /// or if the response status is not successful
     pub async fn send<R, T>(&self, method: Method, url: Url, body: Option<&R>) -> Result<T>
     where
         R: Serialize + ?Sized + Send + Sync + Debug,
@@ -39,7 +71,19 @@ impl RequestHandler {
         self.handle_response(response).await
     }
 
-    /// Handles the Response for a given HTTP request
+    /// Handles the Response for a given HTTP request, attempting to deserialize the response body into the requested type
+    ///
+    /// # Type Parameters
+    /// * `T` - The type to which the response body should be deserialized
+    ///
+    /// # Arguments
+    /// * `response` - The `reqwest::Response` received from the HTTP request
+    ///
+    /// # Returns
+    /// A `Result` wrapping the deserialized data if the response is parsed successfully
+    ///
+    /// # Errors
+    /// Returns an error if deserialization fails or if the response status indicates a failure (e.g., 404 Not Found)
     async fn handle_response<T: for<'de> Deserialize<'de>>(&self, response: Response) -> Result<T> {
         let status: StatusCode = response.status();
         let path: String = response.url().path().to_string();
