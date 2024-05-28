6a5689430cd483e55573239daba3991ae0400930
David Kathoh <mudavidkat@gmail.com>
2024-05-28 12:35:31 +0200

    add remove_address_from_webhook example

---
+++ examples/remove_address_from_webhook.rs
@@ -0,0 +1,17 @@
+use helius::types::Cluster;
+use helius::Helius;
+
+#[tokio::main]
+async fn main() {
+    let api_key: &str = "your_api_key";
+    let cluster: Cluster = Cluster::MainnetBeta;
+
+    let helius: Helius = Helius::new(api_key, cluster).unwrap();
+
+    let webhook_id = "your_webhook_id";
+    let addresses_to_remove = ["your_address1".to_string(), "your_address2".to_string()];
+    let response = helius
+        .remove_addresses_from_webhook(webhook_id, &addresses_to_remove)
+        .await;
+    println!("Addresses removed  {:?}", response);
+}
