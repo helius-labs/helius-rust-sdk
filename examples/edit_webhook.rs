3747b1f010319a7c05256bf378be4a09153b178c
David Kathoh <mudavidkat@gmail.com>
2024-05-28 12:32:11 +0200

    add edit_webhook example

---
+++ examples/edit_webhook.rs
@@ -0,0 +1,23 @@
+use helius::types::{Cluster, EditWebhookRequest, TransactionType, WebhookType};
+use helius::Helius;
+
+#[tokio::main]
+async fn main() {
+    let api_key: &str = "your_api_key";
+    let cluster: Cluster = Cluster::MainnetBeta;
+
+    let helius: Helius = Helius::new(api_key, cluster).unwrap();
+    let request = EditWebhookRequest {
+        webhook_id: "your_webhook_id".to_string(),
+        webhook_url: "your_webhook".to_string(),
+        transaction_types: vec![TransactionType::Any],
+        account_addresses: vec!["your_addresses".to_string()],
+        webhook_type: WebhookType::Enhanced,
+        auth_header: None,
+        txn_status: Default::default(),
+        encoding: Default::default(),
+    };
+
+    let response = helius.edit_webhook(request).await;
+    println!("Webhook edited {:?}", response);
+}
