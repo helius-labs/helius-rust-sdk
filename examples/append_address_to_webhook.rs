60c4d27a536d079bc4dd449d9a1702ea8baac95b
David Kathoh <mudavidkat@gmail.com>
2024-05-28 12:36:23 +0200

    add append_address_to_webhook example

---
+++ examples/append_address_to_webhook.rs
@@ -0,0 +1,15 @@
+use helius::types::Cluster;
+use helius::Helius;
+
+#[tokio::main]
+async fn main() {
+    let api_key: &str = "your_api_key";
+    let cluster: Cluster = Cluster::MainnetBeta;
+
+    let helius: Helius = Helius::new(api_key, cluster).unwrap();
+
+    let webhook_id = "your_webhook_id";
+    let new_addresses = ["your_address1".to_string(), "your_address2".to_string()];
+    let response = helius.append_addresses_to_webhook(webhook_id, &new_addresses).await;
+    println!("Addresses appended  {:?}", response);
+}
