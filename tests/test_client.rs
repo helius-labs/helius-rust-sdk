7ed0366f9e302a49344e3626112032fb1589c537
Evan <0xIchigo@protonmail.com>
2024-05-30 02:17:51 -0400

    Add test_creating_new_async_client_success

--- tests/test_client.rs
+++ tests/test_client.rs
@@ -13,3 +13,16 @@ fn test_creating_new_client_success() {
     let helius: Helius = result.unwrap();
     assert_eq!(helius.config.api_key, api_key);
 }
+
+#[test]
+fn test_creating_new_async_client_success() {
+    let api_key: &str = "valid-api-key";
+    let cluster: Cluster = Cluster::Devnet;
+
+    let result: Result<Helius> = Helius::new_with_async_solana(api_key, cluster);
+    assert!(result.is_ok());
+
+    let helius: Helius = result.unwrap();
+    assert_eq!(helius.config.api_key, api_key);
+    assert!(helius.async_rpc_client.is_some());
+}
