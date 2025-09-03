2d556b2301ac5badac01da50bc18fd966e696fbf
0xIchigo <evan@helius.xyz>
2025-09-03 12:18:17 -0400

    Formattooorrr

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,11 +1,11 @@
 pub mod enhanced_transaction_types;
 pub mod enhanced_websocket;
 pub mod enums;
-pub mod options;
 pub mod inner;
+pub mod options;
 
 pub use self::enhanced_transaction_types::*;
 pub use self::enhanced_websocket::*;
 pub use self::enums::*;
-pub use self::options::*;
 pub use self::inner::*;
+pub use self::options::*;
